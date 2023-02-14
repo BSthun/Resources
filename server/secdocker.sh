886f4083621aee69390fc4d5ae9de1ff0726e23e
Thun (MacBook Pro) <bsthun@gmail.com>
2023-02-15 02:57:34 +0700

    Add server setup script (2)

---
+++ server/secdocker.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Check <name>, <no> is provided
+if [ -z "$1" || -z "$2" ]; then
+	echo "Providing <name> and <no> is required"
+	exit 1
+fi
+
+# Check <no> is a number
+if ! [[ "$2" =~ ^[0-9]+$ ]]; then
+	echo "<no> must be a number"
+	exit 1
+fi
+
+# Define variables
+export DBASE=/var/sec/engine.$1
+export IFACE=enp3s0
+export PORT_START=$((6000+($2*100)))
+export PORT_END=$((6099+($2*100)))
+export BINDING_ADDRESS=172.20.10.$(($2+2))
+
+function setup_iptables() {
+	export RULE="PREROUTING -i $IFACE -p tcp --match multiport --dports $PORT_START:$PORT_END -j DNAT --to-destination $BINDING_ADDRESS:$PORT_START-$PORT_END"
+	iptables -t nat -C $RULE || iptables -t nat -A $RULE
+}
+
+# Check $DBASE.data exists
+if [ ! -d "$DBASE.data" ]; then
+	(
+		# Wait for docker daemon to start
+		while [ ! -S "$DBASE.sock" ]; do
+			sleep 1
+		done
+
+		# Create docker network
+		export DOCKER_HOST=unix://$DBASE.sock
+		docker network create \
+			--driver=bridge \
+			--subnet=172.19.$2.0/24 \
+			--gateway=172.19.$2.1 \
+			--ip-range=172.19.$2.0/24 \
+			--opt com.docker.network.bridge.name=docker1.$1 \
+			--opt com.docker.network.bridge.enable_icc=true \
+			--opt com.docker.network.bridge.host_binding_ipv4=$BINDING_ADDRESS \
+			br0
+		setup_iptables
+	) &
+else
+	setup_iptables
+fi
+
+# Start docker daemon
+dockerd --bridge=docker0 --data-root=$DBASE.data --exec-root=$DBASE.exec --host=unix://$DBASE.sock --pidfile=$DBASE.pid --ip 172.20.10.2
--- server/setup.sh
+++ server/setup.sh
@@ -1,37 +1,67 @@
+#!/bin/bash
+
+# Setup hostname
+sed -i "s/debian/$(hostname)/g" /etc/hosts
+
 # Customize bashrc
 echo "export PS1='\[\033[36m\]\h \[\033[1;32m\][ \w ]▶ \[\033[00m\]'"  >> ~/.bashrc
-echo "bind '"\e[A": history-search-backward'"  >> ~/.bashrc
+echo "bind '"\\e[A": history-search-backward'" >> ~/.bashrc
+echo "bind '"\\e[B": history-search-forward'" >> ~/.bashrc
 
+# Add aliases and functions
+tee -a ~/.bashrc <<EOF
+alias l="ls -al"
+alias gt="gotop -c monokai"
+alias ct="systemd-cgtop -m"
+function de() {
+	docker exec -it $1 /bin/bash
+}
+function dh() {
+	if [ -z "\$1" ]; then
+		export DOCKER_HOST=unix:///var/run/docker.sock
+		return
+	fi
 
+	if [ ! -S "/var/sec/engine.\$1.sock" ]; then
+		echo "Socket /var/sec/engine.\$1.sock does not exist"
+		return
+	fi
+
+	export DOCKER_HOST=unix:///var/sec/engine.\$1.sock
+}
+function cdh() {
+	if [ -z "\$DOCKER_HOST" ]; then
+		echo "unix:///var/run/docker.sock"
+		return
+	fi
+
+	echo "\$DOCKER_HOST"
+}
+EOF
 
 # Install essential packages
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 sudo apt-get update
-sudo apt-get install htop nload iotop wget curl ca-certificates gnupg lsb-release
+sudo apt-get install -y htop nload iotop curl wget iptables-persistent ca-certificates gnupg lsb-release apt-transport-https
 
 # Install Docker
 sudo mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Install Gotop
 wget -O /tmp/gotop.deb https://github.com/cjbassi/gotop/releases/download/3.0.0/gotop_3.0.0_linux_amd64.deb
 sudo dpkg -i /tmp/gotop.deb
 
-# Setup aliases
-alias l="ls -al"
+# Enable IPv4 forwarding
+sysctl -w net.ipv4.ip_forward=1
+sysctp -p
 
-# Setup functions
-function de() {
-	docker exec -it $1 /bin/bash
-}
+# Cleanup
+sudo rm -r /tmp/*
 
-function dh() {
-	if [ -z "$1" ]; then
-		export DOCKER_HOST=unix:///var/run/docker.sock
-		return
-	fi
-
-	export DOCKER_HOST=unix:///var/run/docker.$1.sock
-}
+# Source bashrc
+source ~/.bashrc
